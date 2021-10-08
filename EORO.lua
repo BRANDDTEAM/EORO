b7fd68811e484a37cd32b2b4af88a933e725b5aa
BRANDDTEAM <79874762+BRANDDTEAM@users.noreply.github.com>
2021-10-08 16:44:24 +0300

    Add files via upload

--- EORO.lua
+++ EORO.lua
@@ -8704,6 +8704,7 @@ keyboard.inline_keyboard = {
 {
 {text = 'م5', callback_data=msg.sender_user_id_.."/help5"},
 },
+}
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
